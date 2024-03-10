c4471b4d0ebc962ae71f672a03ec937bd944840f
striver_xin <striver.xin@gmail.com>
2024-03-10 22:58:46 +0800

    add cell world

--- stage1_simple/world.py
+++ stage1_simple/world.py
@@ -1,18 +1,30 @@
 import os
+import sys
 import time
-os.environ['DISPLAY'] = ':20'
 import gymnasium as gym
 import cv2
 
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-class AtariWorld(object):
-    def __init__(self, agent=None) -> None:
+sys.path.append('.\\')
+import stage1_simple.Xenv as xenv
+
+class BaseWorld(object):
+    def __init__(self, agent=None, os_type='win') -> None:
         self.agent = agent
         self.cur_action = 0
+        self.os_type = os_type
         # 创建环境
-        self.env = gym.make("BattleZone",render_mode="rgb_array",obs_type='grayscale')
+        if self.os_type == 'linux-remote':
+            os.environ['DISPLAY'] = ':20'
+            self.render_mode = 'rgb_array'
+        else:
+            self.render_mode = 'human'
+        self.init_env()
+    
+    def init_env(self):
+        self.env = gym.make("BattleZone",render_mode=self.render_mode,obs_type='grayscale')
 
     def run(self, max_frame=1000, render_mode='human', fps=20):
         #重置环境
@@ -29,12 +41,12 @@ class AtariWorld(object):
         for frame_cnt in range(1,max_frame+1):
             if render_mode == 'human':
                 start_time = time.time()
-                rgb_array = self.env.render()
-                resized_array = rgb_array 
-                #resized_array = cv2.resize(rgb_array, (210, 160))  # 调整图像大小
-                cv2.imshow('BattleZone', cv2.cvtColor(resized_array, cv2.COLOR_RGB2BGR))
-                cv2.waitKey(1)
-                            # 计算帧时间并延迟以达到目标帧率
+                if self.os_type == 'linux-remote':
+                    rgb_array = self.env.render()
+                    resized_array = rgb_array 
+                    #resized_array = cv2.resize(rgb_array, (210, 160))  # 调整图像大小
+                    cv2.imshow('BattleZone', cv2.cvtColor(resized_array, cv2.COLOR_RGB2BGR))
+                    cv2.waitKey(1)
                 if frame_cnt % 100 == 0:
                     print('cur fps:%s'%((frame_cnt-last_frame)/(time.time()-period_time_start)))
                     last_frame = frame_cnt
@@ -45,6 +57,7 @@ class AtariWorld(object):
             if terminated or truncated:
                 observation, info = self.env.reset()
             if render_mode == 'human':
+                # 计算帧时间并延迟以达到目标帧率
                 frame_time_elapsed = time.time() - start_time
                 if frame_time_elapsed < frame_time:
                     time.sleep(frame_time - frame_time_elapsed)
@@ -56,9 +69,26 @@ class AtariWorld(object):
     def stop(self):
         self.env.close()
 
+class AtariWorld(BaseWorld):
+    def __init__(self, agent=None, os_type='win') -> None:
+        super().__init__(agent,os_type)
+    
+    def init_env(self):
+        self.env = gym.make("BattleZone",render_mode=self.render_mode,obs_type='grayscale')
+
+
+class CellWorld(BaseWorld):
+    def __init__(self, agent=None, os_type='win') -> None:
+        super().__init__(agent,os_type)
+    
+    def init_env(self):
+        self.env = gym.make("stage1/GridWorld-v0",render_mode=self.render_mode)
+
+
 if __name__ == '__main__':
-    atari = AtariWorld()
-    atari.run(render_mode='human')
+    # world = AtariWorld()
+    world = CellWorld(os_type='linux-remote')
+    world.run(render_mode='human')
 
 
 
